4f027af3a91457639216e5653de01c09006e567f
Jason Andorfer <jason.andorfer@gmail.com>
2015-04-14 00:15:31 -0500

    Tweak description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jandorfer/basic-signaling-service "0.1.0-SNAPSHOT"
-  :description "A handler to enable basic peer exchange signaling, intended for use with RFCPeerConnection."
-  :url "https://github.com/jandorfer/basic-signalling-service"
+  :description "A clojure library for websocket-based message exchange, intended for use for RFCPeerConnection setup signaling."
+  :url "https://github.com/jandorfer/basic-signaling-service"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
